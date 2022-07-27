660783672fb40b9a778db507f76e7eb6efd9d6bc
jayl2sw <jayl2sf@gmail.com>
2022-07-27 22:39:06 +0900

    UPDATE lunch controller entity, repository, controller, dto for lucnh #lee
    Former-commit-id: 018cebdb9a1564b04af42ebf99aec5c338d6a999

---
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/UserRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:473fb407ad910d684186f4efddc1d19a09cb7f815a5e216c98253c01595f3f2a
+size 236
--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/LunchService.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/LunchService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc96d73df7eb01777b41201651835f08378cb238d3818b13b1b66050786a330c
-size 328
+oid sha256:ceaba7a8835f85dcc919441df0c8a4d39d445f47003e6ce2aef97536e34fe6f7
+size 357
