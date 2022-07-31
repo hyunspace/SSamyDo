3d551ddbb5934b99e1cf10ffa82c50e69d2aa249
이재준 <jayl2cu@gmail.com>
2022-07-31 17:57:34 +0900

    ADD services add all services without UserService need to add Authorization on  UserService add LunchController need some Data for Testing #lee
    Former-commit-id: e686a6347345ee22e8daceecfa3d35bd8fe2f3d2

--- Front-End/src/screens/Main.js
+++ Front-End/src/screens/Main.js
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e79a830be812ac89fc06b60d12b5c661e847a7b05597252e8d98acf3b4cd6e99
-size 412
+import { View, Text, Button } from "react-native";
+import TimeLine from "../components/main/TimeLine";
+import TodoList from "../components/main/TodoList";
+import styles from "../../app.module.css";
+
+export default function Main({ navigation }) {
+  return (
+    <View style={[styles.border, styles.center]}>
+      <Text>Main</Text>
+      <TodoList navigation={navigation} />
+      <TimeLine />
+    </View>
+  );
+}
