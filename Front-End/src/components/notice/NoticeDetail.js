7849278fd991fca2479324b53c7f4803f5bef369
hyunspace <at.hyunspace@gmail.com>
2022-08-04 02:03:02 +0900

    working on timeine and checking grids
    Former-commit-id: 312507c04a869b1ce06e79f89b4dc1098fc87add

--- Front-End/src/components/notice/NoticeDetail.js
+++ Front-End/src/components/notice/NoticeDetail.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85e809aae2d322004fea3fc3a1d410d46a5efd666164e6ea18396f96282dde54
-size 346
+oid sha256:b4b9f0d12de0a5424b1bc120a2847f6a7a90ea4071bb61b2901638b94947700b
+size 1485
