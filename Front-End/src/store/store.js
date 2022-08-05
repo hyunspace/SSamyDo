21c215b8f0919116506c5d1c15f12343c90e31bc
전현우 <soshot22@naver.com>
2022-08-05 10:30:58 +0900

    UPDATE FRONTEND calendar store change #jeon
    Former-commit-id: 6d79ba33aabf791596bc52c06c3f55e3b84231df

--- Front-End/src/store/store.js
+++ Front-End/src/store/store.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad2247288aa4c9f35426d2adb6b9ddce9faae82d27cc65288f907d99bc9cafaa
-size 276
+oid sha256:119c09240a988134596c8ac8aa06c2e9774970bc8d27a1d69606a5692510a36c
+size 783
