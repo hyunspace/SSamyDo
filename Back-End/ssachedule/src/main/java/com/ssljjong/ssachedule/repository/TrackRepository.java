c3a416cd1249dd3d741df0d2f61edb5d8803123f
이재준 <jayl2cu@gmail.com>
2022-08-07 23:52:40 +0900

    UPDATE repostories all repositories return Entity now and entities will be changed into Dto by services #lee
    Former-commit-id: f4cc4c4d385235239f98a4f0e68edb9c8a3c92a6

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/TrackRepository.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/repository/TrackRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f30ade0b43d1e66ff17b5c52a30a0b22efa7dce07695e3c1b14c0f65e5baef4
-size 455
+oid sha256:31af10797304b8bbb37767735866c1e4d04740a672af7e60fde1dcf6c2681de2
+size 451
