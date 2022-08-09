46c31738c9cab02f36d0e03e7eadbc699d367dae
이재준 <jayl2cu@gmail.com>
2022-08-09 16:54:39 +0900

    UPDATE controller give default data for FE #lee
    Former-commit-id: 16a434dc39b6c64a66c6d26e8fbc7f4c256d460f

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/TodoController.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/controller/TodoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e23d190f31095725e32077dcbcc56e87f8807341e971d1dc8108db31a736a5d8
-size 318
+oid sha256:24810e0fc5279177d9eba9c18307b66bf4b819042dd7fa3c659757f8d99df817
+size 1680
--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/NoticeService.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/service/NoticeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d55b9fbbcfafa4855b8dc98f61c21b7d2980f58b9887c018047b5e73f6e3bb0
-size 391
+oid sha256:929383c827f939029bc4b8cec38a28a36f74ff86ac57fab4a7bc3a382e9ccc4e
+size 469
