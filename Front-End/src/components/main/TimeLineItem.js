ef723c80e6bf411430c9af30ff2a31b7914ca739
ms_google <ms_google@naver.com>
2022-08-13 16:34:03 +0900

    FIX todolist todolist axios #park
    Former-commit-id: 043dbb99b58b5e615d5aca1049c49a4bfa72ce23

--- Front-End/src/components/main/TimeLineItem.js
+++ Front-End/src/components/main/TimeLineItem.js
@@ -1,10 +1,3 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-
-export default function TimeLineItem() {
-  return (
-    <View>
-      <Text>TimeLineItem</Text>
-    </View>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d42511e8eff1c471f5108e197acb279e1d403d549e661aaa2b5aea759e48199
+size 206
