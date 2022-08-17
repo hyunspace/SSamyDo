1e8925020914dc7dc767c0a5be43949321bf0069
33j073 <33j073@gmail.com>
2022-08-17 23:35:57 +0900

    UPDATE timeline css change timeline css px to percent #kim
    Former-commit-id: 9076440d33ee44ceefde59e9993a6fb5a2f528c9

--- Front-End/src/components/myPage/Alarm.js
+++ Front-End/src/components/myPage/Alarm.js
@@ -1,11 +1,3 @@
-
-import { View, Text } from "react-native";
-import styles from "../../../app.module.css";
-
-export default function Alarm() {
-  return (
-    <View>
-      <Text>Alarm.js</Text>
-    </View>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8083390172a517457016712dc027b590892f70f0d50f2af545e1cc89aaf9d4ed
+size 194
