0387ee183f929cd95ae67c42b92fceb1247ab5ce
ms_google <ms_google@naver.com>
2022-08-18 09:11:43 +0900

    FIX todo filter, CSS #park
    Former-commit-id: de072c1682a0593413f96eaf1561b01b697ecf4a

--- Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/TrackDto.java
+++ Back-End/ssachedule/src/main/java/com/ssljjong/ssachedule/dto/TrackDto.java
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0fdc032d6566d7c79838eec855a0779e0b5cee3e7907ad0f4bf4dfb4eb58c5c1
-size 293
+package com.ssljjong.ssachedule.dto;
+
+import lombok.Data;
+
+@Data
+public class TrackDto {
+
+    private Integer id;
+    private String name;
+    private int gi;
+
+    public TrackDto(Integer id, String name, int gi) {
+        this.id = id;
+        this.name = name;
+        this.gi = gi;
+    }
+}
